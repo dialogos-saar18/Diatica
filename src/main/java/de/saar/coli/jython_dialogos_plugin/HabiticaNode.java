d2848d09a03a85fd41c060bf8faf2ad6080cabbd
ugrivous <36850433+ugrivous@users.noreply.github.com>
2019-01-18 00:38:10 +0100

    merge

--- src/main/java/de/saar/coli/jython_dialogos_plugin/HabiticaNode.java
+++ src/main/java/de/saar/coli/jython_dialogos_plugin/HabiticaNode.java
@@ -75,27 +75,7 @@ public class HabiticaNode extends Node {
             System.out.println("Fehler: Bitte überprüfe deine Internetverbindung!");
             return getEdge(1).getTarget();
           }
-<<<<<<< HEAD
         } else if(eingabe.equals("sleep")){
-=======
-        }
-        else if(eingabe.equals("\"test\"")){
-            try{
-                System.out.println("Ich bin in Test.");
-                result = diaticaCo.sendGet(xapiuser, xapikey, "https://habitica.com/api/v3/tasks/cc5d85be-964f-4cd3-a1db-8130958f01ba"); //für GET
-
-                String varName = this.getProperty(RESULT_VAR).toString();
-                Slot var = getSlot(varName);
-                var.setValue(new StringValue(result));
-    
-                return getEdge(0).getTarget();
-              } catch(Exception e) {
-                System.out.println("Fehler: Bitte überprüfe deine Internetverbindung!");
-                return getEdge(1).getTarget();
-            }
-          }
-        else if(eingabe.equals("sleep")){ // TODO sleep zu sleep und wake up 
->>>>>>> 5a7087584ef1d487852046bf5c0c5977b9fa182d
           try{
             String sleepstatus = diaticaCo.sendPost(xapiuser, xapikey,"https://habitica.com/api/v3/user/sleep"); //für POST
             if (sleepstatus.equals("wach")){
